8713e4d8b0002f73cf25d25b18c6f0b450d8150f
Plawan Rath <plawanrath@gmail.com>
2017-07-24 22:24:30 -0500

    initial commit

---
+++ sgbmuseumapp/index.windows.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View, WebView
+} from 'react-native';
+
+class sgbmuseumapp extends Component {
+  render() {
+    return (
+        <WebView source={{ uri:'http://www.cbsatpathy.com' }} scalesPageToFit={true} />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('sgbmuseumapp', () => sgbmuseumapp);
